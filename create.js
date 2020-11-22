9f89c112b8dc349f80a5a6b398d3290c7b53598b
Yoni Kohn <yonikohn@Yonis-MacBook-Pro.local>
2020-11-22 19:15:33 +0200

    Adding our serverless API

---
+++ create.js
@@ -0,0 +1,22 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      // The attributes of the item to be created
+      userId: "125", // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+  console.log({item: params.TableName});
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
